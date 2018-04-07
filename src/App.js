3b8a78719163742af7818a4dd0b55ab75dd9d23f
Chen <czfgreat@gmail.com>
2018-04-07 16:28:59 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import EventComponent from "./EventComponent";
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    };
+
+    handleScroll(event) {
+        console.log('the scroll things', event)
+    };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+          <EventComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/EventComponent.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import classNames from 'classnames';
+import './EventComponent.css';
+
+export default class EventComponent extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+
+        this._onTouchStart = this._onTouchStart.bind(this);
+        this._onTouchMove = this._onTouchMove.bind(this);
+        this._onTouchEnd = this._onTouchEnd.bind(this);
+
+        this.state = {swiped: false, moveY: 0, showHead: false, loading: false};
+        this._swipe = {};
+        this.minDistance = 50;
+        this.maxDistance = 100;
+    }
+
+    _onTouchStart(e) {
+        if(this.state.loading) return;
+        const touch = e.touches[0];
+        this._swipe = {y: touch.clientY};
+        this.setState({swiped: false});
+        console.log('touch start:y:' + touch.clientY);
+    }
+
+    _onTouchMove(e) {
+
+        if(this.state.loading) return;
+        const touch = e.changedTouches[0];
+        // console.log(e);
+        console.log('touch move:y' + touch.clientY);
+        // this._swipe.y=touch.clientY;
+
+
+        let absY = touch.clientY - this._swipe.y;
+        if (absY > this.maxDistance) absY = this.maxDistance;
+
+        if (absY < 0) return;
+        this.setState({
+            moveY: absY
+        });
+        if (e.changedTouches && e.changedTouches.length) {
+
+            this._swipe.swiping = true;
+        }
+    }
+
+    _onTouchEnd(e) {
+        if(this.state.loading) return;
+        const touch = e.changedTouches[0];
+        const absY = touch.clientY - this._swipe.y;
+        if (this._swipe.swiping && absY > this.minDistance) {
+            this.props.onSwiped && this.props.onSwiped();
+            this.setState({swiped: true, moveY: 50, loading: true});
+            setTimeout(() => {
+                this.setState({
+                    moveY: 0,
+                    loading: false
+                });
+                //  setTimeout(()=>this.setState({showHead:false}),1000);
+            }, 6000);
+        } else {
+            this.setState({moveY: 0});
+        }
+        this._swipe = {};
+        console.log('touch end:y:' + touch.clientY);
+        console.log('touch end:y:' + absY);
+    }
+
+    render() {
+        return (
+
+            <div>
+                <div className={classNames({refreshHead: true, gone: this.state.moveY === 0})}
+                     style={{height: this.state.moveY + 'px', textAlign: 'bottoms'}}>
+
+                        <img src={'https://assets.imedao.com/images/logos/logo.2015.png'}
+                             className={classNames({headLoadingIcon: true, playAnimation: this.state.loading})}
+                             style={{marginTop: this.state.moveY / 2 + 'px'}}/>
+                        <span  className={classNames({headLoadingText: true })}  style={{marginTop: this.state.moveY / 2 + 'px'}}  > loading</span>
+
+                </div>
+                <div style={{height: '300px', backgroundColor: '#ff0000'}}
+                     onTouchStart={this._onTouchStart}
+                     onTouchMove={this._onTouchMove}
+                     onTouchEnd={this._onTouchEnd}>
+                    {`Component-${this.state.swiped ? 'swiped' : ''}`}
+                </div>
+
+            </div>
+
+    )
+        ;
+    }
+
+}
